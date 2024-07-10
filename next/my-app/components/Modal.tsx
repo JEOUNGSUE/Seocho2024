d0fb7bc5f7b9c9b85f59e60fdb50d6a6b6367b6d
JEOUNGSUE <gimjs211459@gmail.com>
2024-07-10 12:20:48 +0900

    next 2nd

--- next/my-app/components/Modal.tsx
+++ next/my-app/components/Modal.tsx
@@ -1,15 +1,4 @@
-import clsx from 'clsx';
-import Image from 'next/image';
-import { Photo } from '@/lib/placeholder';
-
-export default function ImageViewer({
-  photo,
-  isModal = false,
-}: {
-  photo: Photo;
-  isModal?: boolean;
-}) {
-  return ('use client';
+'use client';
 
 import clsx from 'clsx';
 import { useRouter } from 'next/navigation';
@@ -65,13 +54,6 @@ export default function Modal({
       >
         {children}
       </div>
-    </div>
-  );
-}
-    <div className={clsx({ 'm-5 p-5 border-2': !isModal })}>
-      <Image src={photo.url} width={600} height={600} alt={photo.title} />
-
-      <h3 className='text-xl text-center mt-3'>{photo.title}</h3>
     </div>
   );
 }
